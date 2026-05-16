163a52317f6aeb529bf67c04748ca53d37bf1247
wefma <wefmaika1985@gmail.com>
2026-05-16 15:14:22 +0000

    thscoreboardの権限を減らす

--- docker-entrypoint-initdb.d/02_add_authority.sql
+++ docker-entrypoint-initdb.d/02_add_authority.sql
@@ -3,4 +3,3 @@ ALTER USER thscoreboard CREATEDB;
 
 GRANT ALL PRIVILEGES ON DATABASE thscoreboard TO thscoreboard;
 GRANT USAGE, CREATE ON SCHEMA public TO thscoreboard;
-ALTER SCHEMA public OWNER TO thscoreboard;
